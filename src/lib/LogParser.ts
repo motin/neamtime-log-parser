8bfe5ebd1098b6b046d2291d7934de99e48e8181
Motin <motin@motin.eu>
2021-01-04 18:04:14 +0200

    Also detect/parse dates that are before 1999 and after 2020

--- src/lib/LogParser.ts
+++ src/lib/LogParser.ts
@@ -103,9 +103,9 @@ export class LogParser {
   // todo: dyn load appr-tokens
   {
     const regexDetectYmd =
-      "(1999|2000|2001|2002|2003|2004|2005|2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020)-\\d+-\\d+";
+      "(\\d{4})-\\d+-\\d+";
     const regexDetectdmY =
-      "[^\\s\\>>]*-.*-(1999|2000|2001|2002|2003|2004|2005|2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020)";
+      "[^\\s\\>>]*-.*-(\\d{4})";
     const regexDetectHis = "(\\d+:\\d+:\\d+)";
     const regexDetectHcoloniAcceptingApproxToken =
       "(\\d+:([^\\s\\-,:]*|ca|appr))";
